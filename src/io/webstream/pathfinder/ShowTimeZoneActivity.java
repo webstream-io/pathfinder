d9f60470e07fd781edcc4858fe1b3707225addf7
Thongam Deepak Singh <deepak@webstream.io>
2013-07-18 14:56:48 +0530

    Updated the code to display local time for any selected time zone IDs

--- src/io/webstream/pathfinder/ShowTimeZoneActivity.java
+++ src/io/webstream/pathfinder/ShowTimeZoneActivity.java
@@ -1,5 +1,10 @@
 package io.webstream.pathfinder;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,15 +20,15 @@ public class ShowTimeZoneActivity extends Activity {
 	    Intent intent = getIntent();
 	    String itemValue = intent.getStringExtra(DisplayTimezoneActivity.EXTRA_MESSAGE);
 
-	    // Create the text view
-	    TextView textView = new TextView(this);
-	    textView.setTextSize(40);
-	    textView.setText(itemValue);
-
-	    // Set the text view as the activity layout
-	    setContentView(textView);
-
-			
+	    // Set the activity_show_time_zone layout
+	    setContentView(R.layout.activity_main);
+	    TextView textView = (TextView) findViewById(R.id.show_tz);
+	    
+	    Date date = new Date();
+	    DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	    df.setTimeZone(TimeZone.getTimeZone(itemValue));
+	    
+	    textView.setText("Time zone ID:   "+itemValue+"\n"+"Time zone name:   "+TimeZone.getTimeZone(itemValue).getDisplayName()+"\n"+"Current time:"+df.format(date));	
 	}
 
 }
